eb05ebf063c5967cd09237b01b283885ea9c0138
L0tte <l.kaatee@hotmail.com>
2022-04-07 13:56:31 +0200

    remove duplicate code

--- TODO_analyses.py
+++ TODO_analyses.py
@@ -1,6 +1,10 @@
 # Implement linguistic analyses using spacy
 # Run them on data/preprocessed/train/sentences.txt
+from importlib.resources import path
+from nbformat import read
+from sklearn import preprocessing
 
+from collections import Counter
 import spacy
 
 
@@ -9,8 +13,8 @@ import spacy
 # For other models and languages check: https://spacy.io/models
 nlp = spacy.load('en_core_web_sm')
 
-file_name = r"C:\Users\janus\OneDrive\Msc Artificial Intelligence\Jaar 1\P5\NLP\assignments\intro2nlp_assignment1_code\intro2nlp_assignment1_code\data\preprocessed\train\sentences.txt"
-data = open(file_name, encoding="utf8").read()
+file = f"data/preprocessed/train/sentences.txt"
+data = open(file, encoding="utf8").read()
 # Let's run the NLP pipeline on our test input
 doc = nlp(data)
 
@@ -118,88 +122,11 @@ for i in most_common_tag:
     relative = round(i[1] / num_types,2)
     print(f"{i[0]}, {uni}, {i[1]}, {relative}, {words}, {least[0]}")
 
-    
-#########print tokenization
-# for token in doc:
-#     print(token.i, token, token.idx)
-# print()
 
-##########sentence segmentation
-# sentences = doc.sents
-# for sentence in sentences:
-#     print()
-#     print(sentence)
-#     for token in sentence:
-#         print(token.text)
-#
-# for token in doc:
-#     print(token.text, token.lemma_)
+"""
+N-Grams
 
-#######pos tagging
-# for token in doc:
-#     print(token.text, token.pos_, token.tag_)
+"""
 
 
-###########lemmatization
-# for token in doc:
-#     print(token.text, token.lemma_)
 
-#########named entity
-# for ent in doc.ents:
-#     print(ent.text, ent.label_)
-
-############display - not working
-# from spacy import displacy
-# displacy.render(doc, jupyter=False, style='ent')
-
-#############token frequencies
-
-
-# Our test input is the first paragraph of https://spacy.io/usage/linguistic-features
-# Let's run the NLP pipeline on our test input
-
-from collections import Counter
-word_frequencies = Counter()
-
-for sentence in doc.sents:
-    words = []
-    for token in sentence:
-        # Let's filter out punctuation
-        if not token.is_punct:
-            words.append(token.text)
-    word_frequencies.update(words)
-
-print(word_frequencies)
-
-#############
-num_tokens = len(doc)
-num_words = sum(word_frequencies.values())
-num_types = len(word_frequencies.keys())
-
-# print(num_tokens, num_words, num_types)
-# print(f"num_tokens: {num_tokens} num_words: {num_words} num_types: {num_types}")
-
-#assignment
-#PART A
-#1
-# Process the dataset using the spaCy package and extract the following information:
-# Number of tokens:
-# Number of types:
-# Number of words:
-# Average number of words per sentence:
-# Average word length:
-#
-# Provide the definition that you used to determine words:
-
-def assignment_1():
-    print(f"num_tokens: {num_tokens} num_types: {num_types} num_words: {num_words}")
-
-    average_words = 5
-    print(f"Average number of words per sentence:{average_words} ")
-
-def assignment_2():
-    for token in doc:
-        print(token.text, token.pos_, token.tag_)
-
-# assignment_1()
-assignment_2()
